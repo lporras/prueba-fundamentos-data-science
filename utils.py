028e1c946347224a0501f78266382c5bb5ba7e66
Luis Porras <lporras16@gmail.com>
2019-09-04 16:34:53 -0400

    agregar funcion binarizadora en utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File: utils.py
+Author: Luis Alfredo Porras Páez
+Email: lporras16[at]gmail[dot]com
+Github: https://github.com/lporras
+Description: Funciones Útiles para los desafios de la Prueba
+"""
+
 #Importar librerias
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -19,4 +32,36 @@ def visualize_rows (df):
             plt.title(i)
             plt.xlabel("")
     plt.tight_layout()
+
+# Función Binarizadora de columnas de un Data Frame
+def binarize_columns(dataframe, columns):
+    """
+    Definición:
+    Dado un DataFrame y una lista de columnas
+    Binariza cada columna en una nueva columna b_variable
+    tomando como criterio:
+    - Asignar 1 a las categorías minoritarias.
+    - Asignar 0 al resto.
+    Parámetros de ingreso:
+    - dataframe: Tipo de dato: DataFrame.  Base de Datos a modificar (Pandas DataFrame)
+    - columns: Tipo de dato: Array. Lista de nombres de las columnas a binarizar
+    Retorno:
+    - Nuevo DataFrame
+    """
+    tmp_dataframe = dataframe.copy()
+    for variable in columns:
+        binary_column = f"b_{variable}"
+        print(f"Analizando Datos de la variable: {variable}")
+        variable_value_counts = dataframe[variable].value_counts()
+        print(variable_value_counts)
+        max_variables_count = variable_value_counts.max()
+        print(f"El valor más frecuente tiene: {max_variables_count} registros")
+        mayor_variable = variable_value_counts[variable_value_counts == max_variables_count].index[0]
+        print(f"El valor más frecuente es: {mayor_variable}")
+        tmp_dataframe[binary_column] = np.where(tmp_dataframe[variable] == mayor_variable, 0, 1)
+        print(f"Analizando Datos Binarizados de la variable: {variable}")
+        print(tmp_dataframe[binary_column].value_counts())
+    return tmp_dataframe
+
+
 
